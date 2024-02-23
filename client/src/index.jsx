081792c92d304a78765b3f022eb650b61e39ed00
SANMANOK <santiagojoseortega301@gmail.com>
2024-02-23 11:30:48 -0300

    front

---
+++ client/src/index.jsx
@@ -0,0 +1,21 @@
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './main.css'
+import store from './redux/store/store.js'
+import { createRoot } from 'react-dom/client'
+
+const rootElement = document.getElementById('root')
+
+createRoot(rootElement).render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
